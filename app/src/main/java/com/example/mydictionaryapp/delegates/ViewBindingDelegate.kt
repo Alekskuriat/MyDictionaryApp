681d6b5898ef3680aed3f3de0f00c422e0091634
Aleks <kuriatae@mail.ru>
2021-11-21 18:00:18 +0700

    исправлена ошибки с ViewBindingDelegate

--- app/src/main/java/com/example/mydictionaryapp/delegates/ViewBindingDelegate.kt
+++ app/src/main/java/com/example/mydictionaryapp/delegates/ViewBindingDelegate.kt
@@ -11,14 +11,28 @@ import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
 fun <V : ViewBinding> Fragment.viewBinding(bind: (view: View) -> V) =
-    ViewBindingDelegate(bind)
+    ViewBindingDelegate(this, bind)
 
 
 class ViewBindingDelegate<V : ViewBinding>(
+    fragment: Fragment,
     private val bind: (view: View) -> V
 ) : ReadOnlyProperty<Fragment, V> {
 
     private var viewBinding: V? = null
+
+    init {
+        fragment.viewLifecycleOwnerLiveData.observe(fragment) {
+            it.lifecycle.addObserver(object : LifecycleObserver {
+                @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+                fun destroyed() {
+                    android.os.Handler(Looper.getMainLooper()).post {
+                        viewBinding = null
+                    }
+                }
+            })
+        }
+    }
 
     override fun getValue(thisRef: Fragment, property: KProperty<*>): V {
         return viewBinding ?: run {
