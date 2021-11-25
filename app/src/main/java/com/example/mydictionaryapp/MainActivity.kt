eb4238c4f18805d0f78dfd19ffcf6f167b2f2bf5
Aleks <kuriatae@mail.ru>
2021-11-25 20:41:34 +0700

    Реализован BlurEffect для ImageView

--- app/src/main/java/com/example/mydictionaryapp/MainActivity.kt
+++ app/src/main/java/com/example/mydictionaryapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mydictionaryapp
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -11,19 +12,42 @@ import com.github.terrakok.cicerone.NavigatorHolder
 import com.github.terrakok.cicerone.Router
 import com.github.terrakok.cicerone.androidx.AppNavigator
 import org.koin.android.ext.android.inject
-import android.content.Intent
+import android.os.Build
+import android.view.View
+import android.view.animation.DecelerateInterpolator
+import androidx.annotation.RequiresApi
+import androidx.core.animation.doOnEnd
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private val navigator = AppNavigator(this, R.id.container)
     private val cicerone = Cicerone.create()
-    private val navigatorHolder : NavigatorHolder by inject()
-    private val router : Router by inject()
+    private val navigatorHolder: NavigatorHolder by inject()
+    private val router: Router by inject()
 
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setSplashScreen(INTERPOLATOR_DURATION, DecelerateInterpolator(INTERPOLATOR_FACTOR))
         setSupportActionBar(findViewById(R.id.toolbar))
         savedInstanceState ?: router.newRootScreen(DictionaryScreen().show())
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun setSplashScreen(duration: Long, interpolator: DecelerateInterpolator) {
+        splashScreen.setOnExitAnimationListener { splashView ->
+            val slideLeft = ObjectAnimator.ofFloat(
+                splashView,
+                View.TRANSLATION_Y,
+                0f,
+                -splashView.height.toFloat()
+            ).apply {
+                this.interpolator = interpolator
+                this.duration = duration
+                doOnEnd { splashView.remove() }
+                start()
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -46,6 +70,11 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onPause() {
         super.onPause()
         navigatorHolder.removeNavigator()
+    }
+
+    companion object {
+        private const val INTERPOLATOR_FACTOR = 2f
+        private const val INTERPOLATOR_DURATION = 1000L
     }
 }
 
--- app/src/main/java/com/example/mydictionaryapp/view/detailsScreen/DetailsFragment.kt
+++ app/src/main/java/com/example/mydictionaryapp/view/detailsScreen/DetailsFragment.kt
@@ -1,15 +1,24 @@
 package com.example.mydictionaryapp.view.detailsScreen
 
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import coil.api.load
+import coil.request.LoadRequest
 import coil.transform.CircleCropTransformation
 import com.example.dictionaryapp.model.entities.DataModel
 import com.example.mydictionaryapp.R
 import com.example.mydictionaryapp.databinding.FragmentDetailsBinding
 import com.example.mydictionaryapp.delegates.viewBinding
 import com.example.mydictionaryapp.domain.database.HistoryEntity
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.channels.ticker
 
 
 class DetailsFragment : Fragment(R.layout.fragment_details) {
@@ -54,11 +63,38 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
             translation.text = translationWord
             descriptionImageview.load("https:$imageUrl") {
                 crossfade(CROSSFADE)
-                placeholder(R.drawable.ic_launcher_foreground)
                 transformations(CircleCropTransformation())
                 size(SIZE_IMAGE)
+                target(
+                    onStart = {
+                        descriptionImageview.setImageResource(R.drawable.ic_launcher_foreground)
+                    },
+                    onSuccess = {
+                        descriptionImageview.setImageDrawable(it)
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                            CoroutineScope(IO).launch {
+                                for (i in BLUR_STEP downTo ZERO) {
+                                    if (i == ZERO) {
+                                        descriptionImageview.setRenderEffect(null)
+                                        break
+                                    }
+                                    descriptionImageview.setRenderEffect(blurEffect(i.toFloat()))
+                                    delay(BLUR_DURATION)
+                                }
+                            }
+                        }
+                    },
+                    onError = {
+                        descriptionImageview.setImageResource(R.drawable.ic_baseline_image_not_supported_24)
+                    }
+                )
             }
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun blurEffect(a: Float): RenderEffect {
+        return RenderEffect.createBlurEffect(a, a, Shader.TileMode.CLAMP)
     }
 
     companion object {
@@ -66,6 +102,10 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
         const val HISTORY = "history"
         private const val CROSSFADE = 1000
         private const val SIZE_IMAGE = 1000
+        private const val BLUR_DURATION = 100L
+        private const val BLUR_STEP = 15
+        private const val ZERO = 0
+
         fun newInstance(data: DataModel): Fragment =
             DetailsFragment().apply {
                 arguments = Bundle().apply { putParcelable(KEY, data) }
